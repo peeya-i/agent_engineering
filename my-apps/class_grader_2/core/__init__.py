23fe8508c8e1fcb0aa3d5edbcdb14db190f45e89
peeya-i <pi3860@gmail.com>
2026-09-01 23:18:38 -0700

    Move class grader to another repository.

--- my-apps/class_grader_2/core/__init__.py
+++
@@ -1,5 +0,0 @@
-from core.grader import Grader
-from core.storage import ScoreStorage
-from core.models import SubmissionRecord, EvaluationResult, StudentSummary, CriterionResult
-
-__all__ = ["Grader", "ScoreStorage", "SubmissionRecord", "EvaluationResult", "StudentSummary", "CriterionResult"]
